07a9f7be8641dfb7ecb062f8f87b4d0a84bbe300
tmusimesabaoi4i <tmu9837473yohei@gmail.com>
2021-10-10 10:02:14 +0900

    Sound modification and debugging

--- prog/ex1.m
+++ prog/ex1.m
@@ -6,32 +6,44 @@ clc;
 clear;
 
 %% File name
+% filename = 'cos wave';
 filename = 'sample';
 
 %% Read audio file
 [y,fs] = audioread(['../' filename '.wav']);
+% fs = 44.1e3;
+% t = 0:1/fs:1-1/fs;
+% y = 1/2 * cos(2*pi*2e3*t -pi/4) + 3/2 *cos(2*pi*10e3*t +pi/3) + cos(2*pi*15e3*t);
 
-%% Make limit of plot
-lim = { [-10 10] , [-25 100] , [-125 25]};
+%% Make limit of plot and Font size
+lim = { [-1 1] , [0 0.05] , [-0.75 0.75]};
+setting_fontsize = 18;
 
 %% Make time array
 T = length(y)/fs;
 t = 0 : 1/fs : T-1/fs;
 
 %% Get amplitude and theta
-Y = fft(X);
+Y = fft(y);
+L = length(Y);
 amplitude = abs(Y/L);
-theta = angle(Y/L);
 h_amplitude = amplitude(1:L/2+1);
-h_theta = theta(1:L/2+1);
 h_amplitude(2:end-1) = 2*h_amplitude(2:end-1);
-h_theta(2:end-1) = 2*h_theta(2:end-1);
+
+re = real(Y/L);
+im = imag(Y/L);
+
+tol = 5e-3;
+im(amplitude < tol) = 0;
+
+theta = atan(im./re);
+h_theta = theta(1:L/2+1);
 
 %% Make frequency array
 f = linspace(0,fs/2,length(h_amplitude));
 
 %% Make figure
-figure('position', [0, 0, 300*16/9, 300]);
+figure('position', [0, 0, 600*16/9, 600]);
 
 %% Time domain plot
 subplot(3,1,1);
@@ -46,7 +58,7 @@ ax.FontSize = setting_fontsize;
 %% Amplitude plot
 subplot(3,1,2);
 plot(f/1e3,h_amplitude);
-ylabel('');
+ylabel('amplitude');
 xlabel('frequency [kHz]');
 xlim([f(1)/1e3 f(end)/1e3]);
 ylim(lim{2});
@@ -55,8 +67,8 @@ ax.FontSize = setting_fontsize;
 
 %% Theta plot
 subplot(3,1,3);
-plot(f/1e3,h_theta);
-ylabel('');
+plot(f/1e3,h_theta/pi);
+ylabel('theta / \pi');
 xlabel('frequency [kHz]');
 xlim([f(1)/1e3 f(end)/1e3]);
 ylim(lim{3});
@@ -65,6 +77,6 @@ ax.FontSize = setting_fontsize;
 
 %% Save as png
 fig = gcf;
-exportgraphics(fig, [ '../img' filename '.png' ] , 'Resolution', 500);
+exportgraphics(fig, [ '../img/' filename '.png' ] , 'Resolution', 500);
 
 %% END
